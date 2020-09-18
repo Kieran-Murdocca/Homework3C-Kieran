c05d9495b0856120b9c8d1a9f844ff277c59ce6d
KieranMurdocca <kkm5754@psu.edu>
2020-09-18 15:59:48 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,21 @@
+// Author: Kieran Murdocca kkm5754@psu.edu
+// Collaborator: Peter Schulman pks5481@psu.edu
+// Section 004
+// Homework 3
+
+#include <stdio.h>
+
+int digit_sum(int n) {
+  if (n==0)
+    return 0;
+  else if (n<0)
+    return 0;
+  return (n % 10 + digit_sum(n/10));
+}
+
+int main() {
+  int num= (scanf("Enter an int: "));
+  int result = digit_sum(num);
+  printf("sum of digits of %d is %d\n.", num, result);
+  return 0;
+}
